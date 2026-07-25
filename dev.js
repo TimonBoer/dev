afd20316f6ebe48287c7448627ad8a1e85d40af3
Timon Boer <timonboer05@gmail.com>
2026-07-25 11:24:04 +0200

    overzichtelijker

--- dev.js
+++ dev.js
@@ -11,6 +11,32 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+const commands = [
+  {
+    name: 'get state',
+    command: ['/sbin/hdparm', '-C', '/dev/sda']
+  },
+  {
+    name: 'spin down',
+    command: ['/sbin/hdparm', '-y', '/dev/sda']
+  },
+  {
+    name: "sync hdd -> ssd",
+    command: [
+      '/usr/bin/rsync', '-arv',
+      '--exclude', '*/sd', '--exclude', '*/extra meuk', '--delete',
+      '/mnt/sda/shared/timon/fotos', '/home/timon/hdd-backup/timon', '-n'
+    ]
+  },
+  {
+    name: "sync ssd -> hdd",
+    command: [
+      '/usr/bin/rsync', '-arv', '--delete',
+      '/home/timon/hdd-backup/timon/fotos', '/mnt/sda/shared/timon', '-n'
+    ]
+  }
+]
+
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname })
@@ -18,31 +44,12 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   const body = req.body;
-  let command = '';
   const html = fs.readFileSync('index.html', {root: __dirname }).toString();
-  if (body.cmd == 0) { //get state
-    command = 'sudo hdparm -C ' + body.drive;
-  } else if (body.cmd == 1) { //set state
-    command = 'sudo hdparm -y ' + body.drive;
-  } else if (body.cmd == 2) { //sync hdd -> ssd
-    command = `sudo rsync -arv --exclude "*/sd" --exclude "*/extra meuk" --delete "/mnt/sda/shared/afbeeldingen" "/home/timon/hdd-backup"`;
-  } else if (body.cmd == 3) { //sync ssd -> hdd
-    command = `sudo rsync -arv --exclude "*/sd" --exclude "*/extra meuk" --delete "/home/timon/hdd-backup/afbeeldingen" "/mnt/sda/shared"`;
-  } else if (body.cmd == 4) { //dry run hdd -> ssd
-    command = `sudo rsync -arvn --exclude "*/sd" --exclude "*/extra meuk" --delete "/mnt/sda/shared/afbeeldingen" "/home/timon/hdd-backup"`;
-    let stdout = execSync(command).toString();
-    res.send(html + '<p>' + command + '</p><p style="white-space: pre-line">' + stdout + '</p>' + '<form action="" method="post"><button name="cmd" value=2>do it</button></form>');
-    return;
-  } else if (body.cmd == 5) { //dry run ssd -> hdd
-    command = `sudo rsync -arvn --exclude "*/sd" --exclude "*/extra meuk" --delete "/home/timon/hdd-backup/afbeeldingen" "/mnt/sda/shared"`;
-    let stdout = execSync(command).toString();
-    res.send(html + '<p>' + command + '</p><p style="white-space: pre-line">' + stdout + '</p>' + '<form action="" method="post"><button name="cmd" value=3>do it</button></form>');
-    return;
-  }
+  const cmd = commands[2];
+  
+  const { stdout, stderr } = await execFileAsync('sudo', cmd.command);
 
-  let stdout = execSync(command).toString();
-
-  res.send(html + '<p>' + command + '</p><p style="white-space: pre-line">' + stdout + '</p>');
+  res.send(html + '<p>' + cmd.command + '</p><p style="white-space: pre-line">' + stdout.toString() + '</p>');
 })
 
 app.listen(port, () => {
