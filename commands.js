3c68ccd0af0e58736fde87718a417a0dbff541f4
TimonBoer <timonboer05@gmail.com>
2026-07-28 23:25:33 +0200

    pfff

--- commands.js
+++ commands.js
@@ -34,7 +34,7 @@ const commands = [
     hasDryRun: true,
     command: [
       '/usr/bin/rsync', '-arv',
-      '--filter=merge hdd-sdd_filter.txt', '--delete',
+      '--filter=merge hdd-ssd_filter.txt', '--delete',
       path.join(ssdPath, 'shared'), hddPath
     ]
   },
