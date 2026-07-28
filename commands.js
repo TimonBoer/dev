4911a5efc7174536411573cea371f7993ef61284
TimonBoer <timonboer05@gmail.com>
2026-07-28 22:04:06 +0200

    filter fix

--- commands.js
+++ commands.js
@@ -25,7 +25,7 @@ const commands = [
     hasDryRun: true,
     command: [
       '/usr/bin/rsync', '-arv',
-      '--filter="merge filter.txt"', '--delete',
+      '--filter=merge filter.txt', '--delete',
       path.join(hddPath, '/'), path.join(ssdPath, 'hdd-backup')
     ]
   },
@@ -34,7 +34,7 @@ const commands = [
     hasDryRun: true,
     command: [
       '/usr/bin/rsync', '-arv',
-      '--filter="merge filter.txt"', '--delete',
+      '--filter=merge filter.txt', '--delete',
       path.join(ssdPath, '/hdd-backup/'), hddPath
     ]
   },
